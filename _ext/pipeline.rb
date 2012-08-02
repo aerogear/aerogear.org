3cec763d62ded9db9730bbe29cc5228e716d6f5d
kborchers <kris.borchers@gmail.com>
2012-08-02 16:19:37 -0500

    First attempt at wrapping a layout around the asciidoc content

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -3,5 +3,6 @@ Awestruct::Extensions::Pipeline.new do
   # extension Awestruct::Extensions::Posts.new( '/news' ) 
   extension Awestruct::Extensions::Posts.new('/guides')
   extension Awestruct::Extensions::Indexifier.new
+  helper Awestruct::Extensions::Partial
 end
 
