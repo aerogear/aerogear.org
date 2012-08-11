d89358113965f8301591aadd84ad7c0c8745bdf9
Douglas Campos <qmx@qmx.me>
2012-08-11 04:57:15 -0300

    adding asciidoc rendering support

---
+++ _plugins/asciidoc.rb
@@ -0,0 +1,23 @@
+require 'open3'
+module Jekyll
+  class AsciiDoc < Converter
+    safe true
+
+    def matches(ext)
+      ext =~ /asciidoc/
+    end
+
+    def output_ext(ext)
+      '/index.html'
+    end
+
+    def convert(content)
+      cmd = "asciidoc -s -b html5 -a pygments -a icons -o - -"
+      Open3.popen3(cmd) do |stdin, stdout, _|
+        stdin.puts content
+        stdin.close
+        out = stdout.read
+      end
+    end
+  end
+end
