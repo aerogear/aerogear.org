b1e3c27f8a11845eee19a586503e900306a9d5a1
corinnekrych <corinnekrych@gmail.com>
2014-03-18 15:42:25 +0100

    allow include in asciidoctor

--- _plugins/asciidoc.rb
+++ _plugins/asciidoc.rb
@@ -12,7 +12,7 @@ module Jekyll
     end
 
     def convert(content)
-      Asciidoctor.load(content, {:attributes => {'imagesdir' => '../'}}).render
+      Asciidoctor.load(content, :safe => 0, :attributes => {'imagesdir' => '../', 'allow-uri-read' => true}).render
     end
   end
 end
