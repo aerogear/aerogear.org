8024b2535d784832f155de408ff625f88a342ea7
Lukas Fryc <lukas@fryc.eu>
2015-01-28 12:17:55 +0100

    add site.baseurl where it was missing

--- js/jbossorg-tabzilla.js
+++ js/jbossorg-tabzilla.js
@@ -1,3 +1,5 @@
+---
+---
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
@@ -347,7 +349,7 @@ Tabzilla.content =
 +'  <p class="overview"> Like the project? It’s part of the community of Red Hat projects. Learn more about Red Hat and our open source communities:</p>'
 +'  <div class="row-fluid">'
 +'    <span class="span4 middlewarelogo">'
-+'      <img src="/img/RHJB_Middleware_Logotype.png" alt="Red Hat JBoss MIDDLEWARE" />'
++'      <img src="{{ site.baseurl }}/img/RHJB_Middleware_Logotype.png" alt="Red Hat JBoss MIDDLEWARE" />'
 +'    </span>'
 +'    <span class="span4">'
 +'      <ul class="level1">'
@@ -586,7 +588,7 @@ function renderTabzilla( projectName , projectId, fullWidth ) {
     */
 
     // Getting HTML tab content from remote source.
-    var wrapper = $.ajax({url:"/partials/tabcontent.html",
+    var wrapper = $.ajax({url:"{{ site.baseurl }}/partials/tabcontent.html",
       dataType:'html'
     });
 
