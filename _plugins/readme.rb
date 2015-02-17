6196c58542914d9f8c5a0b7fbedf36233b83baf4
Lukas Fryc <lukas@fryc.eu>
2015-02-17 10:22:16 +0100

    load the cookbook lists from the github readmes

--- _plugins/readme.rb
+++ _plugins/readme.rb
@@ -8,29 +8,40 @@ module Readme
   class Generator < Jekyll::Generator
 
     def generate(site)
-      # feed_xml = diskcache.cache('readme') do
-      url = 'https://api.github.com/repos/aerogear/aerogear-android-cookbook/readme'
-      http = Curl.get( url ) do |http|
-        http.headers['User-Agent'] = 'Jekyll'
+
+      cookbooks = {
+          "android" => "https://api.github.com/repos/aerogear/aerogear-android-cookbook/readme",
+          "ios" => "https://api.github.com/repos/aerogear/aerogear-ios-cookbook/readme",
+          "cordova" => "https://api.github.com/repos/aerogear/aerogear-cordova-cookbook/readme",
+          "windows" => "https://api.github.com/repos/aerogear/aerogear-windows-cookbook/readme",
+          "js" => "https://api.github.com/repos/lfryc/aerogear-js-cookbook/readme"
+      }
+
+      diskcache = Diskcached.new('/tmp/aerogear.site.cache', 3000)
+
+      cookbooks.each do |platform, url|
+        body_str = diskcache.cache('cookbook-' + platform) do
+          puts url
+          http = Curl.get( url ) do |http|
+            http.headers['User-Agent'] = 'Jekyll'
+          end
+          http.body_str
+        end
+
+        json = JSON.parse(body_str)
+        baseurl = File.dirname(json['html_url'])
+        content = Base64.decode64(json['content'])
+
+
+        content = content.gsub(/^.*(?:Cookbook apps|Table of content)([^#]+)(?:##.*$|$)/m, '\1')
+        content = content[0..content.rindex('|')]
+        content = content.gsub(/:heavy_check_mark:/, '<i class="fa fa-check green"></i>')
+        content = content.gsub(/:heavy_minus_sign:/, '<i class="fa fa-remove gray"></i>')
+        content = content.gsub(/\[([^\]]+)\]\(([^\)]+)\)/, '[\1](' + baseurl + '/\2)')
+
+        site.data['cookbooks'] = {} unless site.data['cookbooks']
+        site.data['cookbooks'][platform] = content
       end
-      json = JSON.parse(http.body_str)
-
-      # end
-
-      baseurl = File.dirname(json['html_url'])
-
-      content = Base64.decode64(json['content'])
-      tocIndex = content.index('## Cookbook apps') + 16
-      content = content[tocIndex..-1]
-      nextHeadingIndex = content.index('##') - 1
-      content = content[0..nextHeadingIndex]
-
-      content = content.gsub(/:heavy_check_mark:/, '<i class="fa fa-check green"></i>')
-      content = content.gsub(/:heavy_minus_sign:/, '<i class="fa fa-remove gray"></i>')
-      content = content.gsub(/\[([^\]]+)\]\(([^\)]+)\)/, '[\1](' + baseurl + '/\2)')
-
-      site.data['cookbooks'] = {}
-      site.data['cookbooks']['android'] = content
     end
 
   end
