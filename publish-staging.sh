cfadc76bed0c1c0bdbb3d6e7a906982d2538c277
Jason Madigan <jason@jasonmadigan.com>
2018-06-14 12:39:09 +0100

    Verbose rsync

--- publish-staging.sh
+++ publish-staging.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #scp -r _site/ aerogear@filemgmt.jboss.org:/stg_htdocs/aerogear
-rsync -r _site/ aerogear@filemgmt.jboss.org:/stg_htdocs/aerogear
+rsync -vr _site/ aerogear@filemgmt.jboss.org:/stg_htdocs/aerogear
